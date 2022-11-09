f7c75ffd7bd31db9691e9b9f12df0694753a3193
Artem <temazasport@mail.ru>
2022-11-09 14:08:58 +0800

    task1

---
+++ task1/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите два числа");
+int num1 = Convert.ToInt32(Console.ReadLine());
+int num2 = Convert.ToInt32(Console.ReadLine());
+if(num1>num2){
+    Console.Write("max=");
+    Console.Write(num1);
+}
+else{
+    Console.Write("max=");
+    Console.Write(num2);
+   
+}
