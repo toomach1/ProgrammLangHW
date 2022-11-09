50c12e463e4d2c1587dd03b1ea79e243c3c05395
Artem <temazasport@mail.ru>
2022-11-09 14:54:26 +0800

    task2

---
+++ task2/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Введите три числа");
+int num1 = Convert.ToInt32(Console.ReadLine());
+int num2 = Convert.ToInt32(Console.ReadLine());
+int num3 = Convert.ToInt32(Console.ReadLine());
+int max = 0;
+if(num1>num2){
+    if(num1>num3){
+        max=num1;
+    }
+    else{
+       max=num3;
+        }
+}
+else{if(num2>num3){
+        max=num2;
+    }
+    else{max=num3;
+    }
+}
+Console.Write("max=");
+Console.Write(max);
