a399e39530b69c59115b3e3f75b8975d4d7ab72b
Artem <temazasport@mail.ru>
2022-11-09 16:03:52 +0800

    task4

---
+++ task4/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите число");
+int N = Convert.ToInt32(Console.ReadLine());
+int Nstart=2;
+if(N<2){
+    Console.WriteLine("число не может быть меньше или равно 1");
+}
+else{
+    while(Nstart<=N){
+        Console.Write(Nstart + ", ");
+        Nstart=Nstart+2;
+    }
+}
