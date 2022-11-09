eddda7d2afcc48cb2d55340c4e21f95c1dd0457b
Artem <temazasport@mail.ru>
2022-11-09 15:08:13 +0800

    task3

---
+++ task3/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите число");
+int num1 = Convert.ToInt32(Console.ReadLine());
+if(num1%2==0){
+    Console.WriteLine("четное");
+}
+else{
+    Console.WriteLine("нечетное");
+}
